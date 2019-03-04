45ea7446be91101f0ae7fefb5b3e26b690272eae
CIT\murilop <murilop@ciandt.com>
2019-03-04 17:20:13 -0300

    tes

---
+++ precache-manifest.95abf373fcb9436f3336fb1df2b80e16.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1759c85c69d51a04e57807389ab4524",
+    "url": "/img/miniatura_2168.a1759c85.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a1759c85c69d51a04e57807389ab4524",
+    "url": "/img/miniatura_2166.a1759c85.png"
+  },
+  {
+    "revision": "2cc542f628a320ecb4c3",
+    "url": "/css/chunk-vendors.acf2dc0b.css"
+  },
+  {
+    "revision": "d3cb515cb784fff6a6324d2ccc5bd117",
+    "url": "/img/miniatura_1036.d3cb515c.png"
+  },
+  {
+    "revision": "a6c6e5900cb8f65a5dfe97b84c7b1e32",
+    "url": "/img/miniatura_1225.a6c6e590.png"
+  },
+  {
+    "revision": "2b9639b652a58f7ba1c4ee4447567778",
+    "url": "/img/miniatura_1460.2b9639b6.png"
+  },
+  {
+    "revision": "d9f273b38c9d7fd9c53ac28f925adeb9",
+    "url": "/img/miniatura_510.d9f273b3.png"
+  },
+  {
+    "revision": "a1759c85c69d51a04e57807389ab4524",
+    "url": "/img/miniatura_2167.a1759c85.png"
+  },
+  {
+    "revision": "73c1c91fe942ebf084cf80cd28932c56",
+    "url": "/img/miniatura_473.73c1c91f.png"
+  },
+  {
+    "revision": "023c9fbcdd4af66a9092e3d124dacd49",
+    "url": "/img/miniatura_1386.023c9fbc.png"
+  },
+  {
+    "revision": "a1759c85c69d51a04e57807389ab4524",
+    "url": "/img/miniatura_2171.a1759c85.png"
+  },
+  {
+    "revision": "4c8d8041493527bd36dcc2be59b271b9",
+    "url": "/img/miniatura_1002.4c8d8041.png"
+  },
+  {
+    "revision": "573b91cd331839d6d1bc7211e178e858",
+    "url": "/img/miniatura_489.573b91cd.png"
+  },
+  {
+    "revision": "09861989b9d7ded8955cd2e8b94625bf",
+    "url": "/img/miniatura_716.09861989.png"
+  },
+  {
+    "revision": "df5b10777abaf4d4e2791b015d1a7692",
+    "url": "/img/miniatura_sombra.df5b1077.png"
+  },
+  {
+    "revision": "d192ce6f6cab8899939888368da3558d",
+    "url": "/img/miniatura_1110.d192ce6f.png"
+  },
+  {
+    "revision": "b8a066c2deb5b578f2fb5c565fff7e8d",
+    "url": "/img/miniatura_524.b8a066c2.png"
+  },
+  {
+    "revision": "4c8d8041493527bd36dcc2be59b271b9",
+    "url": "/img/miniatura_1000.4c8d8041.png"
+  },
+  {
+    "revision": "d73db3ee346524a06757857df73d4440",
+    "url": "/img/miniatura_2189.d73db3ee.png"
+  },
+  {
+    "revision": "fad4ae9f07dad8c80f146331f30f4b14",
+    "url": "/img/miniatura_1341.fad4ae9f.png"
+  },
+  {
+    "revision": "ff93b45c841527a15c3214778e741c29",
+    "url": "/img/miniatura_1632.ff93b45c.png"
+  },
+  {
+    "revision": "efd83743b10054544036b791da02938c",
+    "url": "/img/miniatura_1628.efd83743.png"
+  },
+  {
+    "revision": "c06ed59e34f18c16f7e30978476d6376",
+    "url": "/img/miniatura_504.c06ed59e.png"
+  },
+  {
+    "revision": "671627c8ef3c9b05ba786f400278219d",
+    "url": "/img/miniatura_488.671627c8.png"
+  },
+  {
+    "revision": "52e57b20e81a84679c6efcdcdb90c44f",
+    "url": "/img/miniatura_1374.52e57b20.png"
+  },
+  {
+    "revision": "d9b5bae42663b767cb198b0703cc81d7",
+    "url": "/img/miniatura_1182.d9b5bae4.png"
+  },
+  {
+    "revision": "a1759c85c69d51a04e57807389ab4524",
+    "url": "/img/miniatura_2170.a1759c85.png"
+  },
+  {
+    "revision": "b8a066c2deb5b578f2fb5c565fff7e8d",
+    "url": "/img/miniatura_1375.b8a066c2.png"
+  },
+  {
+    "revision": "c5bb29df76997df20dad17fdd043c156",
+    "url": "/img/miniatura_520.c5bb29df.png"
+  },
+  {
+    "revision": "b4be143bfa679d9c84a06c79e1801fb4",
+    "url": "/img/miniatura_1114.b4be143b.png"
+  },
+  {
+    "revision": "571a5e8033c0c3702c44c0fd25e0e0c6",
+    "url": "/img/miniatura_630.571a5e80.png"
+  },
+  {
+    "revision": "67287bb4a7f6ee0a5e49c9f74e363184",
+    "url": "/img/miniatura_458.67287bb4.png"
+  },
+  {
+    "revision": "d3cb515cb784fff6a6324d2ccc5bd117",
+    "url": "/img/miniatura_1376.d3cb515c.png"
+  },
+  {
+    "revision": "023c9fbcdd4af66a9092e3d124dacd49",
+    "url": "/img/miniatura_1215.023c9fbc.png"
+  },
+  {
+    "revision": "f2bdc315c4bf430a4da6b5486dc78076",
+    "url": "/img/miniatura_516.f2bdc315.png"
+  },
+  {
+    "revision": "7fd721a88fca4b9b6a95c645256bcd61",
+    "url": "/img/miniatura_1367.7fd721a8.png"
+  },
+  {
+    "revision": "14867d58f693eb8817f1",
+    "url": "/js/app.96ce6967.js"
+  },
+  {
+    "revision": "2cc542f628a320ecb4c3",
+    "url": "/js/chunk-vendors.4456c74c.js"
+  },
+  {
+    "revision": "47e362fc8772be3044a6e417504a9d84",
+    "url": "/img/miniatura_1116.47e362fc.png"
+  },
+  {
+    "revision": "45459777b47ac8f0eaef9013ef9c39c5",
+    "url": "/img/miniatura_1227.45459777.png"
+  },
+  {
+    "revision": "bc059cfe62c973e626244e1db73bee49",
+    "url": "/img/miniatura_1630.bc059cfe.png"
+  },
+  {
+    "revision": "c5bb29df76997df20dad17fdd043c156",
+    "url": "/img/miniatura_507.c5bb29df.png"
+  },
+  {
+    "revision": "1ecb3faa3dc133d5efa1df6890dd6a36",
+    "url": "/img/miniatura_1400.1ecb3faa.png"
+  },
+  {
+    "revision": "c5bb29df76997df20dad17fdd043c156",
+    "url": "/img/miniatura_518.c5bb29df.png"
+  },
+  {
+    "revision": "16b58443eeaf43b1b50a86d92353eaa4",
+    "url": "/img/miniatura_1112.16b58443.png"
+  },
+  {
+    "revision": "ce7758ac2368b0cf337e9a122dcb1760",
+    "url": "/img/miniatura_508.ce7758ac.png"
+  },
+  {
+    "revision": "4a140cc2a52561ed4ff607887c37f21e",
+    "url": "/img/miniatura_1522.4a140cc2.png"
+  },
+  {
+    "revision": "f889a6b87ff4e2a2317ce68e406fbb9f",
+    "url": "/img/miniatura_1933.f889a6b8.png"
+  },
+  {
+    "revision": "8a855372147cd92a5db84899f4352972",
+    "url": "/img/miniatura_1626.8a855372.png"
+  },
+  {
+    "revision": "e52e46d192c37b7a2921eef479f41ca2",
+    "url": "/img/miniatura_490.e52e46d1.png"
+  },
+  {
+    "revision": "b4be143bfa679d9c84a06c79e1801fb4",
+    "url": "/img/miniatura_1369.b4be143b.png"
+  },
+  {
+    "revision": "14767b72f33cf1d178eb75740c96c21f",
+    "url": "/img/miniatura_1524.14767b72.png"
+  },
+  {
+    "revision": "325782a4e45f9d09e8ca479ad2582553",
+    "url": "/img/miniatura_505.325782a4.png"
+  },
+  {
+    "revision": "54f3173bc05d63a01cb7e5ddc3337897",
+    "url": "/img/miniatura_493.54f3173b.png"
+  },
+  {
+    "revision": "7ec5132f0a77ca94dde9861b0a04e1b2",
+    "url": "/img/miniatura_1079.7ec5132f.png"
+  },
+  {
+    "revision": "bb6a283b51b54e6308cdf5ea69f4752d",
+    "url": "/img/sad.bb6a283b.svg"
+  },
+  {
+    "revision": "7ae8f494642f7b9efaef66afa1fcb915",
+    "url": "/img/image-not-found.7ae8f494.png"
+  },
+  {
+    "revision": "14767b72f33cf1d178eb75740c96c21f",
+    "url": "/img/miniatura_1080.14767b72.png"
+  },
+  {
+    "revision": "f554275a8ce14b5423eb3047aac2b7b2",
+    "url": "/img/miniatura_515.f554275a.png"
+  },
+  {
+    "revision": "b480f6c1716ed6fc2bf73e1beb89f68b",
+    "url": "/img/miniatura_468.b480f6c1.png"
+  },
+  {
+    "revision": "9b9111af90b99aebd2b621ada7ee35eb",
+    "url": "/img/miniatura_1307.9b9111af.png"
+  },
+  {
+    "revision": "da2b52e821d5bd12e8d6877c526db6e0",
+    "url": "/img/miniatura_1390.da2b52e8.png"
+  },
+  {
+    "revision": "360f17c5227a2dc1a3394bd5e17cc803",
+    "url": "/img/miniatura_472.360f17c5.png"
+  },
+  {
+    "revision": "a6c6e5900cb8f65a5dfe97b84c7b1e32",
+    "url": "/img/miniatura_1373.a6c6e590.png"
+  },
+  {
+    "revision": "a1759c85c69d51a04e57807389ab4524",
+    "url": "/img/miniatura_2169.a1759c85.png"
+  },
+  {
+    "revision": "91efce8b1e948299731a8edde2b6a910",
+    "url": "/img/miniatura_451.91efce8b.png"
+  },
+  {
+    "revision": "c7ec970ae554f2b7b57bc9f78eccfb51",
+    "url": "/img/miniatura_1624.c7ec970a.png"
+  },
+  {
+    "revision": "e1e1686ef730fb051e938a38a0133700",
+    "url": "/img/miniatura_469.e1e1686e.png"
+  },
+  {
+    "revision": "94866a0129cec289d89a7af11c31db96",
+    "url": "/img/miniatura_517.94866a01.png"
+  },
+  {
+    "revision": "572fa9f2457dd7c33d3ee8382f0b9d06",
+    "url": "/img/miniatura_1349.572fa9f2.png"
+  },
+  {
+    "revision": "c165857c8442e73a2f4f6a195cdc68b3",
+    "url": "/img/miniatura_851.c165857c.png"
+  },
+  {
+    "revision": "54f3173bc05d63a01cb7e5ddc3337897",
+    "url": "/img/miniatura_494.54f3173b.png"
+  },
+  {
+    "revision": "85a2286cf89ee8bb19065a3c7c0787ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14867d58f693eb8817f1",
+    "url": "/css/app.89ee658f.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,6 @@
+importScripts("/precache-manifest.95abf373fcb9436f3336fb1df2b80e16.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
